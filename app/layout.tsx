ff146a021304c41f56a6a4299eb2fe770a97f218
Gijs Siemer <gijs.siemer@me.com>
2025-09-29 14:12:38 +0200

    Add app/layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'testerrrrr-500f8771',
+  description: 'Next.js app powered by Cloudflare',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
